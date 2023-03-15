b50551dd79363b87be37ba5a283ca6fc02f9b31e
julia <kh_jul@mail.ru>
2023-03-15 10:52:09 +0500

    Шаблонизируй это

--- src/main.js
+++ src/main.js
@@ -1,17 +1,13 @@
-import TripMenuView from './view/menu-view.js';
-import TripFilterView from './view/trip-filter-view.js';
-import { render } from './render.js';
-import EventsPresenter from './presenter/eventsPresenter.js';
+import MainPresenter from './presenter/main-presenter.js';
 
-const pageHeader = document.querySelector('.page-header');
-const pageMain = document.querySelector('.page-main');
-const menuElement = pageHeader.querySelector('.trip-controls__navigation');
-const filterElement = pageHeader.querySelector('.trip-controls__filters');
-const tripEvents = pageMain.querySelector('.trip-events');
+import TravelPointModel from './model/travel-point-model.js';
 
-render(new TripMenuView(), menuElement);
-render(new TripFilterView(), filterElement);
+const pageHeaderElement = document.querySelector('.page-header');
+const pageMainElement = document.querySelector('.page-main');
+const tripEventsElement = pageMainElement.querySelector('.trip-events');
+const tripControlsElement = pageHeaderElement.querySelector('.trip-controls');
 
-const tripPresenter = new EventsPresenter();
+const travelPointModel = new TravelPointModel();
+const tripPresenter = new MainPresenter(tripEventsElement, tripControlsElement);
 
-tripPresenter.init(tripEvents);
+tripPresenter.init(travelPointModel);
---
+++ src/mock/travel-point.js
@@ -0,0 +1,28 @@
+import { getRandomInteger } from '../utils.js';
+import dayjs from 'dayjs';
+import { TYPES, DESTINATIONS } from '../consts.js';
+import { offersArray } from './offers.js';
+
+const generateType = () => {
+  const randomIndex = getRandomInteger(0, TYPES.length - 1);
+
+  return TYPES[randomIndex];
+};
+
+const generatePrice = () => getRandomInteger(10,1000);
+
+export const generateTravelPoint = () => {
+  const start = dayjs().add(getRandomInteger(-1, 0),'day').add(getRandomInteger(-11, 0),'hour').add(getRandomInteger(-30, 0), 'minute');
+  const end = dayjs().add(getRandomInteger(0, 1),'day').add(getRandomInteger(0, 11),'hour').add(getRandomInteger(0, 30), 'minute');
+
+  return {
+    basePrice: generatePrice(),
+    isFavorite: Boolean(getRandomInteger(0,1)),
+    type: generateType(),
+    dateFrom: start,
+    dateTo: end,
+    offers: [...new Set(Array.from({length: getRandomInteger(0, offersArray.length) }, () => getRandomInteger(1, offersArray.length - 1)))],
+    destination: getRandomInteger(1, DESTINATIONS.length),
+  };
+};
+
